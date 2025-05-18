de50647ea502c201b6774eb92ab98d6cda3882af
Genius Graphics & Infotech <geniusgraphicsdev07@gmail.com>
2025-05-19 04:39:29 +0530

    commit https

--- vivavistaukadmin/src/utils/api.js
+++ vivavistaukadmin/src/utils/api.js
@@ -1,3 +1,3 @@
 // export const Base_url = "http://localhost:5001/api";
 // export const Base_url = "https://vivavista-backend.onrender.com/api";
-export const Base_url = "https://api.vivavistavacations.co.uk:5002/api";
+export const Base_url = "https://api.vivavistavacations.co.uk/api";
